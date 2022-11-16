e7098538a764404778e35f484df333c35b96ebc9
akursekova <kursek.alina.jr@gmail.com>
2022-11-16 23:33:09 +0100

    quest app implementation version 05: QuestService corrected: added init() method whoch returns first question Logger added

--- src/main/java/dev/akursekova/app/ApplicationServlet.java
+++ src/main/java/dev/akursekova/app/ApplicationServlet.java
@@ -1,5 +1,9 @@
 package dev.akursekova.app;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+import javax.servlet.ServletConfig;
 import javax.servlet.ServletContext;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
@@ -12,103 +16,79 @@ import java.io.IOException;
 @WebServlet(name = "ApplicationServlet", value = "/quest")
 public class ApplicationServlet extends HttpServlet {
 
-    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        System.out.println("doGet started");
+    private static final Logger LOG = LogManager.getLogger(ApplicationServlet.class);
 
-        ServletContext context = getServletContext();
-        QuestService questService = (QuestService) context.getAttribute("questService");
+    private QuestService questService = null;
+
+    private int numberOfGames = 0;
+
+    @Override
+    public void init(ServletConfig config) throws ServletException {
+        super.init(config);
+        ServletContext context = config.getServletContext();
+        questService = (QuestService) context.getAttribute("questService");
+    }
+
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        LOG.info("doGet started");
         HttpSession session = request.getSession(true);
 
-        //trigger index.jsp for introduction
-        if (session.getAttribute("user") == null){
-            System.out.println("DOGET: this is the beginning of the quest");
+        if (!userNameSpecified(session)){
             request.getRequestDispatcher("/index.jsp").forward(request, response);
+            //System.out.println("user name not specified, return wil be called");
+            LOG.info("userName not specified");
+            return;
         }
 
-        //if first question or restarted quest
-        Question currentQuestion;
-        if (session.getAttribute("currentQuestion") == null || questRestarted(session)){
-            System.out.println("this is the very first question or user restarted the quest");
-            currentQuestion = questService.q7;
-            session.setAttribute("currentQuestion", currentQuestion);
+        if (isFirstQuestion(session) || questRestarted(session)){
+            //System.out.println("first question or restarted game");
+            LOG.info("first question or restarted game");
+            setFirstQuestion(questService, session);
             session.setAttribute("questRestarted", false);
         }
 
-        //not a first question
-        System.out.println("current question = " + session.getAttribute("currentQuestion"));
-        currentQuestion = (Question) session.getAttribute("currentQuestion");
-        System.out.println("current question = " + currentQuestion.text + " " + session.getAttribute("currentQuestion"));
-
+        Question currentQuestion = (Question) session.getAttribute("currentQuestion");
         session.setAttribute("currentAnswers", currentQuestion.answers);
         request.getRequestDispatcher("/question.jsp").forward(request, response);
     }
 
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        System.out.println("doPost started");
+        //System.out.println("doPost started");
+        LOG.info("doPost started");
         HttpSession session = request.getSession(true);
-
         int numberOfGames;
 
-
-        //if user not introduced
-        if (session.getAttribute("user") == null){
-            numberOfGames = 0;
-            String user = request.getParameter("name");
-            String ipAddress = request.getRemoteAddr();
-            initializeStatistics(user, ipAddress, numberOfGames, false, session);
-
-            //to remove
-            System.out.println("DOPOST: this is the beginning of the quest");
-            System.out.println("username specified. Ready to start the quest");
-            System.out.println("it's a new user. GameCounter = " + session.getAttribute("numberOfGames"));
-            System.out.println("numberOfGames = " + numberOfGames);
-            //to remove
-
+        if (!userNameSpecified(session)){
+            initializeStatistics(session, request);
+            //System.out.println("user = " + session.getAttribute("user") + ", session_ID = " + session.getId());
+            LOG.debug("User specified it's name: user = " + session.getAttribute("user") + ", session_ID = " + session.getId());
             response.sendRedirect(request.getContextPath() +"/quest");
-        }
-        //user already introduced himself
-        else {
+        } else {
             String buttonClicked = request.getParameter("answer");
             if (buttonClicked.equals("Answer")){
-                System.out.println("Answer button clicked");
-
                 Question currentQuestion = (Question) session.getAttribute("currentQuestion");
 
                 if (!answerSelected(request)) {
-                    System.out.println("user did not choose anything");
+//                    System.out.println("answer not selected");
+                    LOG.info("answer not selected");
                     request.getRequestDispatcher("/question.jsp").forward(request, response);
                 }
-
 
                 int userChoice = Integer.parseInt(request.getParameter("decision"));
                 currentQuestion = currentQuestion.answers.get(userChoice).nextQuestion;
 
-
-                //To delete
-                System.out.println("new current question = " + currentQuestion.text + " " + currentQuestion);
-                System.out.println("new current question isWin = " + currentQuestion.isWin);
-                System.out.println("new current question isLoose = " + currentQuestion.isLoose);
-                //To delete
-
                 if (gameFinished(currentQuestion)){
-                    System.out.println("numberOfGames from session BEFORE increment= " + session.getAttribute("numberOfGames"));
                     numberOfGames = (int) session.getAttribute("numberOfGames") + 1;
-                    System.out.println("numberOfGames VARIABLE AFTER increment= " + numberOfGames);
                     session.setAttribute("numberOfGames", numberOfGames);
-                    System.out.println("numberOfGames from session AFTER increment= " + session.getAttribute("numberOfGames"));
                 }
 
                 session.setAttribute("currentQuestion", currentQuestion);
                 response.sendRedirect(request.getContextPath() +"/quest");
-            }
-            //if user clicked restart
-            else {
-                System.out.println("Restart button clicked");
+            } else {
                 session.setAttribute("questRestarted", true);
                 response.sendRedirect(request.getContextPath() +"/quest");
             }
-
         }
     }
 
@@ -117,19 +97,44 @@ public class ApplicationServlet extends HttpServlet {
         return questRestarted? true : false;
     }
 
-    private void initializeStatistics(String user, String ipAddress, int numberOfGames, Boolean questRestarted, HttpSession session){
+    private void initializeStatistics(HttpSession session, HttpServletRequest request){
+        numberOfGames = 0;
+        String user = request.getParameter("name");
+        String ipAddress = request.getRemoteAddr();
+
         session.setAttribute("user", user);
         session.setAttribute("ipAddress", ipAddress);
         session.setAttribute("numberOfGames", numberOfGames);
-        session.setAttribute("questRestarted", questRestarted);
+        session.setAttribute("questRestarted", false);
     }
 
     private boolean gameFinished(Question question){
-        return (question.isWin == true || question.isLoose == true)? true : false;
+        return (question.isWin || question.isLoose );
     }
 
     private boolean answerSelected(HttpServletRequest request){
-        return (request.getParameter("decision") == null)? false : true;
+        return (request.getParameter("decision") != null);
+    }
+
+    private boolean isFirstQuestion(HttpSession session){
+        return (session.getAttribute("currentQuestion") == null);
+    }
+
+    private void setFirstQuestion(QuestService questService, HttpSession session){
+        Question currentQuestion = questService.init();
+        session.setAttribute("currentQuestion", currentQuestion);
+    }
+
+    private boolean userNameSpecified(HttpSession session){
+        return (session.getAttribute("user") != null);
     }
 }
+
+
+//todo добавить мапу где буду хранить юзера и кол-во игр ? оставить как есть в сессии?
+//todo подключить логер и попробовать с докером провернуть
+//todo ответ от андрея по КвестСервису
+//todo подключить логгер
+//todo вопрос Антону про память сервера вс храние в сессии
+//todo гитхаб вроде как все ок с файлами
 
--- src/main/java/dev/akursekova/app/QuestService.java
+++ src/main/java/dev/akursekova/app/QuestService.java
@@ -3,70 +3,73 @@ package dev.akursekova.app;
 import java.util.List;
 
 public final class QuestService {
-
         private static QuestService questServiceInstance;
+        protected Question init() {
+                Question q1 = Question.builder()
+                        .text("Your lies have been exposed. You have lost.")
+                        .isLoose(true)
+                        .build();
 
-        Question q1 = Question.builder()
-                .text("Your lies have been exposed. You have lost.")
-                .isLoose(true)
-                .build();
+                Question q2 = Question.builder()
+                        .text("You have not negotiated. You have lost.")
+                        .isLoose(true)
+                        .build();
 
-        Question q2 = Question.builder()
-                .text("You have not negotiated. You have lost.")
-                .isLoose(true)
-                .build();
+                Question q3 = Question.builder()
+                        .text("You have rejected the challenge. You have lost.")
+                        .isLoose(true)
+                        .build();
 
-        Question q3 = Question.builder()
-                .text("You have rejected the challenge. You have lost.")
-                .isLoose(true)
-                .build();
+                Question q4 = Question.builder()
+                        .text("You have been returned home. You won.")
+                        .isWin(true)
+                        .build();
 
-        Question q4 = Question.builder()
-                .text("You have been returned home. You won.")
-                .isWin(true)
-                .build();
+                Question q5 = Question.builder()
+                        .text("You went up to the bridge. Who are you?")
+                        .answers(List.of(
+                                Answer.builder()
+                                        .text("Tell the truth")
+                                        .nextQuestion(q4)
+                                        .build(),
+                                Answer.builder()
+                                        .text("Tell a lie")
+                                        .nextQuestion(q1)
+                                        .build()
+                        ))
+                        .build();
 
-        Question q5 = Question.builder()
-                .text("You went up to the bridge. Who are you?")
-                .answers(List.of(
-                        Answer.builder()
-                                .text("Tell the truth")
-                                .nextQuestion(q4)
-                                .build(),
-                        Answer.builder()
-                                .text("Tell a lie")
-                                .nextQuestion(q1)
-                                .build()
-                ))
-                .build();
+                Question q6 = Question.builder()
+                        .text("You've accepted the challenge. Will you come over the captain and will go up the bridge to him?")
+                        .answers(List.of(
+                                Answer.builder()
+                                        .text("Accept to go up to the bridge")
+                                        .nextQuestion(q5)
+                                        .build(),
+                                Answer.builder()
+                                        .text("Reject to go up to the bridge")
+                                        .nextQuestion(q2)
+                                        .build()
+                        ))
+                        .build();
 
-        Question q6 = Question.builder()
-                .text("You've accepted the challenge. Will you come over the captain and will go up the bridge to him?")
-                .answers(List.of(
-                        Answer.builder()
-                                .text("Accept to go up to the bridge")
-                                .nextQuestion(q5)
-                                .build(),
-                        Answer.builder()
-                                .text("Reject to go up to the bridge")
-                                .nextQuestion(q2)
-                                .build()
-                ))
-                .build();
+                Question q7 = Question.builder()
+                        .text("You've lost your memory. Accept the UFO challenge?")
+                        .answers(List.of(
+                                Answer.builder()
+                                        .text("Accept challenge")
+                                        .nextQuestion(q6)
+                                        .build(),
+                                Answer.builder()
+                                        .text("Reject challenge")
+                                        .nextQuestion(q3)
+                                        .build()
+                        ))
+                        .build();
+                return q7;
+        }
 
-        Question q7 = Question.builder()
-                .text("You've lost your memory. Accept the UFO challenge?")
-                .answers(List.of(
-                        Answer.builder()
-                                .text("Accept challenge")
-                                .nextQuestion(q6)
-                                .build(),
-                        Answer.builder()
-                                .text("Reject challenge")
-                                .nextQuestion(q3)
-                                .build()
-                ))
-                .build();
+
 
         public static QuestService getInstance() {
                 if (questServiceInstance == null){
